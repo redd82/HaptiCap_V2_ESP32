50f3c1056e16b4197029278ad02b81584cd46618
Tristan Daams <t.r.daams@tudelft.nl>
2023-02-06 20:37:23 +0100

    SPIFFS list dir test

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,37 @@ String processor(const String& var){
   return String();
 }
  
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println(" - not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
@@ -103,7 +134,7 @@ void setup(){
     return;
   } else {
         
-    
+     listDir(SPIFFS, "/", 0);
   }
 
   // Connect to Wi-Fi
